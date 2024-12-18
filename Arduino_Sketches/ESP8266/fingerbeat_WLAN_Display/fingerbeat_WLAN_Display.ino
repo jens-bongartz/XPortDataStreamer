bec09a5148607a20d57e6dbeaeb8788b98767d3f
Jens Bongartz <32654669+jens-bongartz@users.noreply.github.com>
2024-12-18 13:51:41 +0100

    Update fingerbeat_WLAN_Display.ino

--- Arduino_Sketches/ESP8266/fingerbeat_WLAN_Display/fingerbeat_WLAN_Display.ino
+++ Arduino_Sketches/ESP8266/fingerbeat_WLAN_Display/fingerbeat_WLAN_Display.ino
@@ -12,30 +12,33 @@
 #define I2C_ADDRESS 0x48   // ADS1115
 
 // Select a Timer Clock
-#define USING_TIM_DIV1                true             // for shortest and most accurate timer
-#define USING_TIM_DIV16               false           // for medium time and medium accurate timer
-#define USING_TIM_DIV256              false            // for longest timer but least accurate. Default
+#define USING_TIM_DIV1                true        // for shortest and most accurate timer
+#define USING_TIM_DIV16               false       // for medium time and medium accurate timer
+#define USING_TIM_DIV256              false        // for longest timer but least accurate. Default
+
+// Wifi einrichten
+const char* ssid = "Raspi-Wifi-2.4";               // Darf bis zu 32 Zeichen haben.
+const char* password = "raspi01-wifi";             // Mindestens 8 Zeichen jedoch nicht länger als 64 Zeichen.
+unsigned long previousMillis = 0;
+WiFiUDP Udp;
+IPAddress unicastIP(192, 168, 1, 2);               // Ziel-IP-Adresse eintragen
+constexpr uint16_t PORT = 8266;     
+
+// Timer einrichten
+ESP8266Timer ITimer;
+
+// ADC einrichten
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
+
+// Display einrichten
+// U8G2_R2 = 180 degrees rotated
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R2,SCL_PIN,SDA_PIN,U8X8_PIN_NONE);
 
 #define faADC 200
 #define packetSize 20
 
-// Wifi Daten
-const char* ssid = "Raspi-Wifi-2.4";                       // Darf bis zu 32 Zeichen haben.
-const char* password = "raspi01-wifi";            // Mindestens 8 Zeichen jedoch nicht länger als 64 Zeichen.
-unsigned long previousMillis = 0;
-
 int packetCounter = 0;
 char packetBuffer[25 * packetSize];
-
-WiFiUDP Udp;
-IPAddress unicastIP(192, 168, 1, 2);                  // Adresse des Esp, welcher als Empfänger der Nachricht dient, eintragen.
-constexpr uint16_t PORT = 8266;     
-
-ESP8266Timer ITimer;
-ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
-
-// U8G2_R2 = 180 degrees rotated
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R2,SCL_PIN,SDA_PIN,U8X8_PIN_NONE);
 
 void setup() {
   Wire.begin();
@@ -71,6 +74,7 @@ void setup() {
     Serial.println("ADS1115 found!");         
     u8g2.drawStr(0,40,"ADS1115 connected");
   }
+  // ADS1115 Parameter einstellen
   adc.setVoltageRange_mV(ADS1115_RANGE_0256);   
   adc.setCompareChannels(ADS1115_COMP_0_1);
   adc.setConvRate(ADS1115_250_SPS);              
